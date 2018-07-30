11846893d2d6c3d77e4fc39440dc195991913e6b
ericLavachery <eric.lavachery@gmail.com>
2018-07-30 15:39:59 +0200

    viré le script custom-select

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// variable pour que les includes sachent dans quel page ils sont (servira pour le menu)
+$teou = 'index';
+?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Home | Melius</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+
+
+
+    </body>
+</html>
