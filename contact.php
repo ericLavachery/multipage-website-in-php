653df6c9e3b5ca5e89091185b44a9b8c78adc773
ericLavachery <eric.lavachery@gmail.com>
2018-07-31 11:40:21 +0200

    ajout form sur branche DEV

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+// variable pour que les includes sachent dans quel page ils sont (servira pour le menu)
+$teou = 'contact';
+?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Contact | Melius</title>
+        <link rel="stylesheet" href="form.css">
+    </head>
+    <body>
+        <?php include 'form.php'; ?>
+        
+        <script src="custom-select.js" charset="utf-8"></script>
+    </body>
+</html>
