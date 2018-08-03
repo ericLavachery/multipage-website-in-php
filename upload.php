f08e4cf4bec236af1c6a72c3522adce30227521a
Claudiu <claudiu.simion.cornea@gmail.com>
2018-08-03 09:57:33 +0200

    upload

---
+++ upload.php
@@ -0,0 +1,23 @@
+<?php
+    include('src/class.upload.php'); //Appel à la classe externe
+    if(isset($_POST["submit"])){ //Si on appuie sur le button submit
+        $image = new upload($_FILES["document"]); //On va chercher le fichier a uploader
+        $extension = ($image->file_src_name_ext); //On va chercher l'$extension du fichier
+        if($extension == "png" OR $extension == "jpg" OR $extension == "jpeg" OR $extension == "gif"){ // On verifie le format du fichier
+            if($image->uploaded) {//il suffit de lire la doc pour vérifier les fonctions
+                $image->file_new_name_body   = ($image->file_src_name);
+                $image->file_auto_rename = true;
+                $image->mime_check = true;
+                $image->process('upload/');
+                if ($image->processed) {
+                    echo 'image uploaded';
+                    $image->clean();
+                }else {
+                    echo 'error : ' . $image->error;
+                }
+            }
+        }else{
+            echo("Mauvais format");
+        }
+    }
+?>
