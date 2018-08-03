2ee8618e47e30cd6465875376433294d014235b9
Claudiu <claudiu.simion.cornea@gmail.com>
2018-08-03 10:36:34 +0200

    bug_fix_template

--- _form.php
+++ _form.php
@@ -1,23 +1,31 @@
+<?php
+    require("upload.php");
+    require("sterilize.php");
+?>
 <div class="col-xs-12 col-sm-6 col-md-6 col-lg-5 col-xl-6 tm-contact-left">
     <h2 class="tm-section-header thin-font col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">Contactez-nous</h2>
 
-    <form action="contact.php" method="post" class="contact-form">
+    <form action="contact.php" method="post" class="contact-form" enctype="multipart/form-data">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 tm-contact-form-full">
-            <div class="form-group letitre">
+            <div class="form-group letitre" >
                 <input type="radio" name="titre" id="Mme" value="Mme"> <label for="Mme">Mme &nbsp;</label>
                 <input type="radio" name="titre" id="Melle" value="Melle"> <label for="Melle">Melle &nbsp;</label>
                 <input type="radio" name="titre" id="Mr" value="Mr"> <label for="Mr">Mr &nbsp;</label>
+                <?php display_error($_POST["titre"]) ?>
             </div>
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 tm-contact-form-left">
             <div class="form-group">
-                <input type="text" id="prenom" name="prenom" class="form-control" placeholder="Prénom :"  required/>
+                <input type="text" id="prenom" name="prenom" value="<?php echo($prenom) ?>" class="form-control" placeholder="Prénom :"  required/>
+                <?php display_error($_POST["prenom"]) ?>
             </div>
             <div class="form-group">
-                <input type="text" id="nom" name="nom" class="form-control" placeholder="Nom :"  required/>
+                <input type="text" id="nom" name="nom" value="<?php echo($nom) ?>" class="form-control" placeholder="Nom :"  required/>
+                <?php display_error($_POST["nom"]) ?>
             </div>
             <div class="form-group">
-                <input type="email" id="email" name="email" class="form-control" placeholder="Email :"  required/>
+                <input type="email" id="email" name="email"  class="form-control" placeholder="Email :"  required/>
+                <?php display_error($_POST["email"]) ?>
             </div>
             <div class="form-group">
                 <select class="form-control" name="objet" id="objet">
@@ -25,19 +33,20 @@
                     <option value="info">Demande d'information</option>
                     <option value="inscription">Inscription</option>
                 </select>
+                <?php display_error($_POST["objet"]) ?>
             </div>
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 tm-contact-form-right">
             <div class="form-group">
                 <textarea id="message" name="message" class="form-control" rows="8" placeholder="Message :" required></textarea>
+                <?php display_error($_POST["message"]) ?>
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 tm-contact-form-full">
             <div class="form-group">
                 <input type="file" id="document" name="document" class="form-control" placeholder="Document :">
             </div>
-            <button type="submit" class="btn submit-btn" id="submit">Envoyer</button>
+            <button type="submit" name="submit" class="btn submit-btn" id="submit">Envoyer</button>
         </div>
     </form>
-
 </div>
