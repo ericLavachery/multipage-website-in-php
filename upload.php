8ef6c0e35d69c1d7e7bbb4157561054c7e263877
Claudiu <claudiu.simion.cornea@gmail.com>
2018-08-06 11:39:56 +0200

    upload_error_messages

--- upload.php
+++ upload.php
@@ -1,24 +1,35 @@
 <?php
     include('src/class.upload.php'); //Appel à la classe externe
-    if(isset($_POST["submit"]) AND !empty($_FILES["document"])){ //Si on appuie sur le button submit
-        print_r($_FILES["document"]);
+    $error_extension = "<div class='alert alert-danger' role='alert'>
+            File invalide.
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                <span aria-hidden='true'>&times;</span>
+            </button>
+        </div>";
+    function validate_extension($extension){
+        if($extension == "png" OR $extension == "jpg" OR $extension == "jpeg" OR $extension == "gif" OR $extension == ""){
+            return(TRUE);
+        }else{
+            return(FALSE);
+        }
+    }
+    if(isset($_POST["submit"])){ //Si on appuie sur le button submit
         $image = new upload($_FILES["document"]); //On va chercher le fichier a uploader
         $extension = ($image->file_src_name_ext); //On va chercher l'$extension du fichier
-        if($extension == "png" OR $extension == "jpg" OR $extension == "jpeg" OR $extension == "gif"){ // On verifie le format du fichier
+        $validate_extension = (validate_extension($extension)) ? "" : $error_extension;
+        if(validate_extension($extension)){ // On verifie le format du fichier
             if($image->uploaded) {//il suffit de lire la doc pour vérifier les fonctions
                 $image->file_new_name_body   = ($image->file_src_name);
                 $image->file_auto_rename = true;
                 $image->mime_check = true;
                 $image->process('upload/');
                 if ($image->processed) {
-                    echo 'image uploaded';
+                    //echo 'image uploaded';
                     $image->clean();
                 }else {
                     echo 'error : ' . $image->error;
                 }
             }
-        }else{
-            echo("Mauvais format");
         }
     }
 ?>
