e8eb0af396086d5746b79732bcb2fce76ff976d0
Claudiu <claudiu.simion.cornea@gmail.com>
2018-08-06 11:43:54 +0200

    validation_new_look

--- sterilize.php
+++ sterilize.php
@@ -1,19 +1,4 @@
 <?php
-    if (isset($_POST["submit"])){
-        $titre = $_POST["titre"];
-        $nom = $_POST["nom"];
-        $prenom = $_POST["prenom"];
-        $email = $_POST["email"];
-        $objet = $_POST["objet"];
-        $message = $_POST["message"];
-    }else{
-        $titre = "";
-        $nom = "";
-        $prenom = "";
-        $email = "";
-        $objet = "";
-        $message = "";
-    }
     function check_input($x){
         if(isset($_POST["submit"]) AND empty($x)){
             return(FALSE);
@@ -21,29 +6,33 @@
             return(TRUE);
         }
     }
+    $error_message = "<div class='alert alert-danger' role='alert'>
+            Email invalide.
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                <span aria-hidden='true'>&times;</span>
+            </button>
+        </div>";
     
-    function display_error($x){
-        if(!check_input($x)){
-            echo("ERORR");
-        }
-    }
-    
-    if(isset($_POST["submit"]) AND check_input($message) AND check_input($email)){
-        $san_titre = check_input($titre) ? filter_var($titre,FILTER_SANITIZE_STRING) : "";
-        $san_nom = check_input($nom) ? filter_var($nom,FILTER_SANITIZE_STRING) : "";
-        $san_prenom = check_input($prenom) ? filter_var($prenom,FILTER_SANITIZE_STRING) : "";
-        $san_email = check_input($email) ? filter_var($email,FILTER_SANITIZE_EMAIL) : "";
-        $san_objet = check_input($objet) ? filter_var($objet,FILTER_SANITIZE_STRING) : "";
-        $san_message = check_input($message) ? filter_var($message,FILTER_SANITIZE_STRING) : "";
-        if(isset($san_titre, $san_nom, $san_prenom, $san_email, $san_objet, $san_message)){
-            $user=["titre" => $san_titre,
-                "nom" => $san_nom,
-                "prenom" => $san_prenom,
-                "email" => $san_email,
+    if(isset($_POST["submit"])){
+        $titre = check_input($_POST["titre"]) ? filter_var($_POST["titre"],FILTER_SANITIZE_STRING) : "";
+        $nom = check_input($_POST["nom"]) ? filter_var($_POST["nom"],FILTER_SANITIZE_STRING) : "";
+        $prenom = check_input($_POST["prenom"]) ? filter_var($_POST["prenom"],FILTER_SANITIZE_STRING) : "";
+        $email = check_input($_POST["email"]) ? filter_var($_POST["email"],FILTER_SANITIZE_EMAIL) : "";
+        $objet = check_input($_POST["objet"]) ? filter_var($_POST["objet"],FILTER_SANITIZE_STRING) : "";
+        $message = check_input($_POST["message"]) ? filter_var($_POST["message"],FILTER_SANITIZE_STRING) : "";
+        $validate_email = (filter_var($email,FILTER_VALIDATE_EMAIL) ? "" : $error_message);
+        if(filter_var($email,FILTER_VALIDATE_EMAIL)){  
+            $user=["titre" => $titre,
+                "nom" => $nom,
+                "prenom" => $prenom,
+                "email" => $email,
                 "date" => date('H:i:s l j/m/Y'),
-                "objet" => $san_objet,
-                "message" => $san_message];
+                "objet" => $objet,
+                "message" => $message];
             print_r($user);
         }
     }
+    
+    
+    
 ?>
