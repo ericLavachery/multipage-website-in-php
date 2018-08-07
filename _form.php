90c5402eaf6f0ff929dc1651c7b2c308f104ddd0
ericLavachery <eric.lavachery@gmail.com>
2018-08-07 10:29:06 +0200

    rien

--- _form.php
+++ _form.php
@@ -58,7 +58,6 @@
     <!-- Form confirmation Modal -->
     <div id="formConfirmModal" class="modal fade" role="dialog">
         <div class="modal-dialog">
-            <!-- Modal content-->
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
