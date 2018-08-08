862830c002426854fbd2750f1ab72141214cefc4
ericLavachery <eric.lavachery@gmail.com>
2018-08-08 16:10:48 +0200

    bug crush

--- _mailbody.php
+++ _mailbody.php
@@ -1,9 +1,11 @@
 <?php
-if ($user["objet"] == 'info') {
-    $mailInfos = "<h3>Votre demande d'information à bien été envoyée</h3>";
-} else {
-    $mailInfos = "<h3>Votre inscription à bien été envoyée</h3>";
+if(isset($_POST["submit"])){
+    if ($user["objet"] == 'info') {
+        $mailInfos = "<h3>Votre demande d'information à bien été envoyée</h3>";
+    } else {
+        $mailInfos = "<h3>Votre inscription à bien été envoyée</h3>";
+    }
+    $mailInfos = $mailInfos . '<p>' . $user["prenom"] . ' ' . $user["nom"] . '<br>Email = ' . $user["email"] . '<br>' . $user["message"] . '</p>';
+    $mailBody = '<!DOCTYPE html><html lang="fr" dir="ltr"><head><meta charset="utf-8"><title>Melius</title></head><body>' . $mailInfos . '</body></html>'
 }
-$mailInfos = $mailInfos . '<p>' . $user["prenom"] . ' ' . $user["nom"] . '<br>Email = ' . $user["email"] . '<br>' . $user["message"] . '</p>';
-$mailBody = '<!DOCTYPE html><html lang="fr" dir="ltr"><head><meta charset="utf-8"><title>Melius</title></head><body>' . $mailInfos . '</body></html>'
 ?>
