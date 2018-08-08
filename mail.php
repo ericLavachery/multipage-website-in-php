2c0ee549e5a6caf1b50f64a449023df2ff0ffb56
Claudiu <claudiu.simion.cornea@gmail.com>
2018-08-08 17:28:30 +0200

    mail_test

--- mail.php
+++ mail.php
@@ -1,7 +1,9 @@
 <?php
     use PHPMailer\PHPMailer\PHPMailer;
     require 'vendor/autoload.php';
-    echo($user_file["error"]);
+    echo("file".$user_file["error"]);
+    echo("user".$user_info["error"]);
+    print_r($user);
     if(isset($_POST["submit"]) AND $user_info["error"] AND $user_file["error"]){
         echo($user_file["error"]);
         print_r($user);
