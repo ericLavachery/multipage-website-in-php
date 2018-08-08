bd33588b3a695fb5b36508e540198874fb6d9098
Claudiu <claudiu.simion.cornea@gmail.com>
2018-08-08 12:24:22 +0200

    bugfix_mail

--- mail.php
+++ mail.php
@@ -13,7 +13,7 @@ $mail->isSMTP();
 // 2 = client and server messages
 $mail->SMTPDebug = 2;
 //Set the hostname of the mail server
-$mail->Host = 'smtp.gmail.com';
+//$mail->Host = 'smtp.gmail.com';
 // use
 $mail->Host = gethostbyname('smtp.gmail.com');
 // if your network does not support SMTP over IPv6
