ae6201fa1cc275b1d84b720834b86bbe79d9cf10
ericLavachery <eric.lavachery@gmail.com>
2018-08-09 14:02:24 +0200

    tablier rendu

--- _modal.php
+++ _modal.php
@@ -7,6 +7,7 @@ if(isset($_POST["submit"])){
             $modalTitle = "Votre inscription à bien été envoyée";
         }
         $modalBody = $user['prenom'] . ' ' . $user['nom'] . '<br>Email = ' . $user['email'] . '<br>' . $user['message'];
+        echo('<script>$("#formConfirmModal").modal("show");</script>');
     }
 }
 ?>
@@ -24,6 +25,3 @@ if(isset($_POST["submit"])){
         </div>
     </div>
 </div>
-<?php
-echo('<script>$("#formConfirmModal").modal("show");</script>');
-?>
