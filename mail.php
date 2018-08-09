c858af061df2e10e4f0d7235d8e832d30b7ee935
Claudiu <claudiu.simion.cornea@gmail.com>
2018-08-09 16:45:53 +0200

    mail_add_cc

--- mail.php
+++ mail.php
@@ -33,7 +33,7 @@
         $mail->addReplyTo($user["email"], ($user["titre"] . $user["nom"] ." ". $user["prenom"]));
         //Set who the message is to be sent to
         $mail->addAddress('melius.contact@gmail.com');
-        $mail->addAddress($user["email"]);
+        $mail->AddCC($user["email"]);
         //Set the subject line
         $mail->Subject = $user["objet"];
         //Read an HTML message body from an external file, convert referenced images to embedded,
