51fac7b8e06597f426f4ff508354a7ed690b112d
ericLavachery <eric.lavachery@gmail.com>
2018-08-09 14:36:49 +0200

    lien logs dans footer

--- _footer.php
+++ _footer.php
@@ -1,7 +1,9 @@
 <!-- footer -->
 <footer class="row tm-footer">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-        <p class="text-xs-center tm-footer-text">Copyright &copy; 2018 BeCode</p>
+        <p class="text-xs-center tm-footer-text">
+            <a href="showlog.php">View form logs</a> &nbsp;|&nbsp; Copyright &copy; 2018 BeCode
+        </p>
     </div>
 </footer>
 <!-- footer -->
