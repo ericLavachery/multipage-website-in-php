764a5288a1c9238b492c83ebb9500d1a0e6e9d89
Claudiu <claudiu.simion.cornea@gmail.com>
2018-08-10 10:52:57 +0200

    bugfix_modal

--- _modal.php
+++ _modal.php
@@ -1,4 +1,22 @@
 <?php
+    $message_send = "
+    <div id='formConfirmModal' class='modal fade'>
+        <div class='modal-dialog'>
+            <div class='modal-content'>
+                <div class='modal-header'>
+                    <button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
+                    <h4 class='modal-title'><?php if (isset($modalTitle)) {echo($modalTitle);} ?></h4>
+                </div>
+                <div class='modal-body'>
+                    <p><?php if (isset($modalBody)) {echo($modalBody);} ?></p>
+                </div>
+                <div class='modal-footer'>
+                    <button type='button' class='btn btn-default' data-dismiss='modal'>Fermer</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    ";
     if ($okThen){
         if ($user['objet'] == 'info') {
             $modalTitle = "Votre demande d'information à bien été envoyée";
@@ -6,41 +24,6 @@
             $modalTitle = "Votre inscription à bien été envoyée";
         }
         $modalBody = $user['prenom'] . ' ' . $user['nom'] . '<br>Email = ' . $user['email'] . '<br>' . $user['message'];
+        echo($message_send);
     }
 ?>
-<!-- Modal -->
-<!--<div id="formConfirmModal" class="modal fade" role="dialog">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title"><?php if (isset($modalTitle)) {echo($modalTitle);} ?></h4>
-            </div>
-            <div class="modal-body">
-                <p><?php if (isset($modalBody)) {echo($modalBody);} ?></p>
-            </div>
-        </div>
-    </div>
-</div>-->
-<div id="formConfirmModal" class="modal fade">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title"><?php if (isset($modalTitle)) {echo($modalTitle);} ?></h4>
-            </div>
-            <div class="modal-body">
-                <p><?php if (isset($modalBody)) {echo($modalBody);} ?></p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
-            </div>
-        </div>
-    </div>
-</div>
-<script type='text/javascript'>
-    function message_sent(){
-        if(<?php $okThen ?>)
-        $('#formConfirmModal').modal('show');
-    }
-</script>;
