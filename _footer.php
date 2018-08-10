8677a2efb54568c537300208e42fd54d20e332b7
ericLavachery <eric.lavachery@gmail.com>
2018-08-10 09:51:56 +0200

    some style

--- _footer.php
+++ _footer.php
@@ -2,7 +2,7 @@
 <footer class="row tm-footer">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
         <p class="text-xs-center tm-footer-text">
-            <a href="showlog.php">View form logs</a> &nbsp;|&nbsp; Copyright &copy; 2018 BeCode
+            <a href="showlog.php">Voir les LOGS du formulaire</a> &nbsp;|&nbsp; Copyright &copy; 2018 BeCode
         </p>
     </div>
 </footer>
